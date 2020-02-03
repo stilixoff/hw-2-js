14267fb52087af3765877fe2aac4461c137f1eec
Oleh Starodubets <stilixoff@gmail.com>
2020-02-04 00:16:21 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+let n, numN; 
+n = prompt(`Введи целое число N`);
+numN = parseInt(n);
+
+while ( isNaN(numN) ) {
+    n = prompt(`Введи целое число N`);
+    if ( n == '') {
+        n = 0;
+    };
+    numN = parseInt(Number.isInteger(n));
+}
+
+
+console.log(numN, typeof(numN));
+
+let m, numM;
+m = prompt(`Введи целое число M`);
+numM = parseInt(m);
+
+while ( isNaN(numM) ) {
+    m = prompt(`Введи целое число M`);
+    if ( m == '') {
+        m = 0;
+    };
+    numM = parseInt(m);
+}
+
+console.log(numM, typeof(numM));
+
+let  confirmType = confirm(`пропускать четные числа? ok = да; отмена = нет`);
+
+console.log(confirmType);
+
+let sum = 0;
+
+if ( confirmType == false) {
+    for ( let i = numN; i < numM; i++  ) {
+        sum += i;
+    }
+} else {
+    for ( let i = numN; i < numM; i++  ) {
+        if (i%2 == 0) continue;
+        sum += i;
+    }
+}
+
+console.log(sum);
+
+document.writeln(`Число N = ${numN} <br\/>  Число M = ${numM} <br\/> Пропускать четные числа? = ${confirmType} <br\/> Сумма от ${numN} до ${numM} = ${sum} `)
